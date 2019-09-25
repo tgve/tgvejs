9ad237bfe7afb2779ef03134991deddacdd90738
Layik <layik.hama@gmail.com>
2019-09-25 14:00:19 +0100

    Refactor and separate showcase/family of eatlases

--- src/components/DeckSidebar/DeckSidebar.jsx
+++ src/components/DeckSidebar/DeckSidebar.jsx
@@ -5,21 +5,19 @@ import {
 } from 'react-bootstrap';
 import { format } from 'd3-format';
 import { Button, KIND, SIZE } from 'baseui/button';
-import { DateTime } from "luxon";
 
 import './DeckSidebar.css';
 import DataInput from '../DataInput';
-import RBDropDown from '../RBDropdownComponent';
 import MapboxBaseLayers from '../MapboxBaseLayers';
 import { summariseByYear, percentDiv } from '../../utils';
 import { XYPlot, LineSeries, XAxis, YAxis, } from 'react-vis';
 import Variables from '../Variables';
-import GenerateUI from '../UI';
 import RBAlert from '../RBAlert';
 import { propertyCount, getPropertyValues } from '../../geojsonutils';
 import Constants from '../../Constants';
 import ColorPicker from '../ColourPicker';
 import Modal from '../Table/Modal';
+import { timeSlider, dropdown } from '../Showcases/Widgets';
 
 const URL = (process.env.NODE_ENV === 'development' ? Constants.DEV_URL : Constants.PRD_URL);
 
@@ -81,8 +79,8 @@ export default class DeckSidebar extends React.Component {
       ['Slight', 'Serious', 'Fatal'])
     // console.log(severity_data);
 
-    const curr_road_types = getPropertyValues({features: data}, "road_type");
-    
+    const curr_road_types = getPropertyValues({ features: data }, "road_type");
+
     return (
       <div className="side-panel-container"
         style={{ marginLeft: !open ? '-320px' : '0px' }}>
@@ -119,77 +117,27 @@ export default class DeckSidebar extends React.Component {
           <div className="side-panel-body">
             <div className="side-panel-body-content">
               {/* range of two values slider is not native html */
-                data && data.length > 1 && 
-                (data[0].properties.date || data[0].properties['YEAR']) && 
-                (DateTime.fromFormat(data[0].properties.date + '', 'dd/MM/yyyy').isValid ||
-                DateTime.fromFormat(data[0].properties['YEAR'] + '', 'dd/MM/yyyy').isValid) &&
-                <GenerateUI
-                  title={
-                    <h5>Year(s): {year ? year : "2009 - 2017"}.
-                    {
-                        year &&
-                        <i style={{ fontSize: '2rem' }}
-                          className="fa fa-trash"
-                          onClick={() => {                            
-                            delete multiVarSelect.date;
-                            typeof (onSelectCallback) === 'function' &&
-                              onSelectCallback(Object.keys(multiVarSelect).length === 0 ?
-                                { what: '' } : { what: 'multi', selected: multiVarSelect })
-                            this.setState({
-                              year: "",
-                              multiVarSelect
-                            })
-                          }} />
-                      }
-                    </h5>
-                  }
-                  sublist={data[0].properties.date ? 
-                    Array.apply(null, {length: 9}).map(Number.call, Number ).map(d => d+2009) :
-                    Array.apply(null, {length: 31}).map(Number.call, Number ).map(d => d+2020)
-                  }
-                  suggested="slider"
-                  onChange={(value) => {                    
-                    multiVarSelect[data[0].properties.date ? 
-                    'date' : 'YEAR'] = new Set([value+""]);
-                    this.setState({
-                      year: value,
-                      multiVarSelect
-                    })
-                    typeof (onSelectCallback) === 'function' &&
-                      onSelectCallback({ selected: multiVarSelect, what: 'multi' })
-                  }}
-                />
+                timeSlider(data, year, multiVarSelect, 
+                  onSelectCallback, (changes) => this.setState(changes))
               }
-              {/* <GenerateUI title="Test" sublist={["one", "two"]} suggested="checkbox" /> */
+              {
                 //only if there is such a property
-                data && data.length > 1 && data[0].properties.road_type &&
-                // TODO: filter all_road_types accoridng to the data 
-                <RBDropDown
-                  title={multiVarSelect.road_type ? multiVarSelect.road_type : "Road Type(All)"}
-                  menuitems={ curr_road_types ?
-                    ["All", ...curr_road_types] : all_road_types}
-                  onSelectCallback={(selected) => {
-                    selected === "All" ? delete multiVarSelect.road_type : 
-                    multiVarSelect.road_type = new Set([selected])
-                    this.setState({ multiVarSelect })
-                    onSelectCallback &&
-                      onSelectCallback(Object.keys(multiVarSelect).length === 0 ?
-                      { what: '' } : { what: 'multi', selected: multiVarSelect })
-                  }} />
+                dropdown(data, multiVarSelect, curr_road_types, all_road_types,
+                  onSelectCallback, (changes) => this.setState(changes))
               }
               <br />
               {/* TODO: generate this declaritively too */}
               {
                 severity_data && severity_data.map(each =>
                   percentDiv(each.x, 100 * each.y / data.length, () => {
-                    multiVarSelect['accident_severity'] && 
-                    multiVarSelect['accident_severity'].has(each.x) ?
-                    delete multiVarSelect['accident_severity'] : 
-                    multiVarSelect['accident_severity'] = new Set([each.x]);
+                    multiVarSelect['accident_severity'] &&
+                      multiVarSelect['accident_severity'].has(each.x) ?
+                      delete multiVarSelect['accident_severity'] :
+                      multiVarSelect['accident_severity'] = new Set([each.x]);
                     this.setState({ multiVarSelect })
                     onSelectCallback &&
                       onSelectCallback(Object.keys(multiVarSelect).length === 0 ?
-                      {what: ''} : { what: 'multi', selected: multiVarSelect})
+                        { what: '' } : { what: 'multi', selected: multiVarSelect })
                   }))
               }
               <hr style={{ clear: 'both' }} />
@@ -202,12 +150,12 @@ export default class DeckSidebar extends React.Component {
                     data && data.length > 0 &&
                     <Variables
                       multiVarSelect={multiVarSelect}
-                      onSelectCallback={(mvs) => {                        
+                      onSelectCallback={(mvs) => {
                         typeof (onSelectCallback) === 'function' &&
                           onSelectCallback(
                             Object.keys(mvs).length === 0 ?
                               { what: '' } : { what: 'multi', selected: mvs })
-                        this.setState({multiVarSelect: mvs})
+                        this.setState({ multiVarSelect: mvs })
                       }}
                       data={data} />
                   }
@@ -341,3 +289,5 @@ export default class DeckSidebar extends React.Component {
     )
   }
 }
+
+
