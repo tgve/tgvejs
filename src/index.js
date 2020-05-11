901603480d545f5cd2a07607037130df6d28b871
Layik Hama <layik.hama@gmail.com>
2020-05-11 17:47:20 +0100

    publish 0.0.21, react-vis css, which will be replaced

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import App from './App';
 // TODO: not the best way to add the react-vis css
-import '../react-vis/dist/style.css';
+import '../../react-vis/dist/style.css';
 
 export default (props) => <App {...props} />;
