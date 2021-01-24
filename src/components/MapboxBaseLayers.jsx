7e96cb912e28473a8b310ad82c958f3a8cff83f7
Layik Hama <layik.hama@gmail.com>
2021-01-24 19:16:05 +0000

    Removing Bootstrap based components

--- src/components/MapboxBaseLayers.jsx
+++ src/components/MapboxBaseLayers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RBDropDown from './RBDropdownComponent';
+import MultiSelect from './MultiSelect';
 
 export default class MapboxBaseLayers extends React.Component {
     constructor(props) {
@@ -23,15 +23,27 @@ export default class MapboxBaseLayers extends React.Component {
         const {onSelectCallback} = this.props;
         
         return(
-            <RBDropDown
-                title={selected === 'dark' ? "Default(dark)" : selected}
-                menuitems={bases}
-                onSelectCallback= {(selected) => {
-                    this.setState({selected});
-                    typeof(onSelectCallback) === 'function' &&
-                    onSelectCallback(selected)
-                }}
-            />
+          <MultiSelect 
+          title={selected === 'dark' ? "Default(dark)" : selected}
+            single={true}
+            values={bases.map(e => ({id:e, value:e}))}
+            onSelectCallback= {(selected) => {
+              const base = selected && selected[0] ?
+              selected[0].value : this.state.selected;
+              this.setState({selected: base});
+              typeof(onSelectCallback) === 'function' && base &&
+              onSelectCallback(base)
+          }}
+          />
+            // <RBDropDown
+            //     title={selected === 'dark' ? "Default(dark)" : selected}
+            //     menuitems={bases}
+            //     onSelectCallback= {(selected) => {
+            //         this.setState({selected});
+            //         typeof(onSelectCallback) === 'function' &&
+            //         onSelectCallback(selected)
+            //     }}
+            // />
         )
     }
 }
