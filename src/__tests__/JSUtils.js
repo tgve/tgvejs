28118cdf1cf17da62474340a36fbbe7d4f2c1be6
Layik Hama <layik.hama@gmail.com>
2021-03-12 21:12:29 +0000

    Merge master tests - no browserrouter

---
+++ src/__tests__/JSUtils.js
@@ -0,0 +1,9 @@
+import {randomToNumber} from '../JSUtils'
+
+test("randomToNumber", () => {
+  expect(randomToNumber(null)).toEqual(0)
+  expect(randomToNumber({})).toEqual(0)
+  const n = 10;
+  const r = randomToNumber(n);
+  expect(r >= 0 && r < n).toBe(true)
+})
