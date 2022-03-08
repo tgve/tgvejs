d2e8fabbb8544ce29012fe3db5cb46f1a9e72fdf
Roly Perera <roly.perera@dynamicaspects.org>
2022-03-08 15:02:55 +0000

    More test config.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom'
+
 configure({ adapter: new Adapter() });
 // plotly & some other package tests error fix
 // https://github.com/plotly/react-plotly.js/issues/115
