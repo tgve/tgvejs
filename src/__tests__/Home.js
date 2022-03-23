4571ed6dcf0f6bf2e5f24fc99deca705f97afe55
Layik Hama <layik.hama@gmail.com>
2022-03-23 10:16:19 +0000

    Refac Welcome in tests

--- src/__tests__/Home.js
+++ src/__tests__/Home.js
@@ -2,19 +2,19 @@ import React from 'react';
 import {shallow} from 'enzyme';
 // import { BrowserRouter } from 'react-router-dom';
 
-import Welcome from '../Welcome';
+import Home from '../home';
 // import DeckGL from 'deck.gl';
-const w = shallow(<Welcome location={{search: null}}/>);
+const w = shallow(<Home location={{search: null}}/>);
 
-test('Welcome shallow and mount', () => {
+test('Home shallow and mount', () => {
   // console.log(w.debug())
   expect(w.find('DeckGL')).not.toBeNull();
   expect(w.find('InteractiveMap')).not.toBeNull();
   expect(w.find("div.loader").length).toBe(1)
 })
 
-test('Welcome child DeckSidebarContainer', () => {
-  const wd = shallow(<Welcome hideCharts={true}/>);
+test('Home child DeckSidebarContainer', () => {
+  const wd = shallow(<Home hideCharts={true}/>);
   const ds = wd.find('div').find('DeckSidebarContainer');
   // console.log(ds.debug());
   expect(ds.props().hideCharts).toBe(true)
