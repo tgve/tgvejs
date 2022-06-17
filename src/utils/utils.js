280558c65ef1da4f4e388412009e654f24af9bdf
Layik Hama <layik.hama@gmail.com>
2022-06-17 10:48:30 +0100

    Minor legend format

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -693,16 +693,34 @@ const generateLegend = (options) => {
   for (var i = 0; i < legendMax; i += 1) {
     legend.push(
       i === 0 ?
-        <i key={i}>{jMin.toFixed(2)}</i>
+        <i key={i}>{nFormatter(jMin)}</i>
         :
         i === (legendMax - 1) ?
-          <i key={i}>{jMax.toFixed(2)}</i>
+          <i key={i}>{nFormatter(jMax)}</i>
           :
           <span key={i} style={{ background: interpolate(i / legendMax) }}>
           </span>
     )
   }
   return legend;
+}
+
+// credit https://so.com/a/9462382/11101153
+function nFormatter(num, digits = 2) {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" }
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup.slice().reverse().find(function(item) {
+    return num >= item.value;
+  });
+  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
 }
 
 /**
