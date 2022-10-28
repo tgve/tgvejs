e48262ddef7913e340b154610eeec8b648abb2b0
Layik Hama <layik.hama@gmail.com>
2022-10-28 16:18:30 +0100

    Update snapshot

--- src/__tests__/ColorPicker.js
+++ src/__tests__/ColorPicker.js
@@ -1,14 +1,15 @@
 
 import * as React from 'react'
 import {render, fireEvent, screen} from '@testing-library/react'
-import ColorPicker from '../components/ColourPicker'
+import ColorPicker from '../components/ColorPicker'
 
 test('shows ColorPicker renders', () => {
   const handleClick = jest.fn()
   const { container } = render(
     <ColorPicker colourCallback={handleClick}/>)
   const colorDivs = container.getElementsByClassName('color-box')
-  expect(screen.queryByText("Color:")).not.toBeNull()
+  // change in UI
+  expect(screen.queryByText("Color:")).toBeNull()
   expect(colorDivs.length).toBe(6);
 
   fireEvent.click(colorDivs[0])
